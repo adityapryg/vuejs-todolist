1937753143ad05c7f3d030362b4378cd757a72a9
Aditya Prayoga <aditya@brainmatics.com>
2025-11-12 10:42:28 +0700

    feat: integrate Tailwind CSS and refactor components
    - Added Tailwind CSS dependencies to package.json.
    - Created a new tailwind.css file for global styles.
    - Replaced Task component with ListItem component in App.vue.
    - Refactored AppHeader component to use Tailwind CSS classes.
    - Created ListItem component to display task items.
    - Removed the old Task component and created a new TaskItem component with Tailwind CSS styling.
    - Updated main.ts to include the new tailwind.css file.
    - Modified vite.config.ts to include Tailwind CSS plugin.

---
+++ src/components/ListItem.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="max-w-[600px] my-8 mx-auto px-4">
+    <ul class="bg-white/80 backdrop-blur-md rounded-2xl p-6 shadow-lg">
+      <TaskItem :isChecked="true"> True Komponen TaskItem </TaskItem>
+      <TaskItem :isChecked="false"> False Komponen Task </TaskItem>
+    </ul>
+  </div>
+</template>
+<script setup lang="ts">
+import TaskItem from './TaskItem.vue'
+</script>
